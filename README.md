485417d101849bec148eb89eeb106cabf4f08ced
sid12214737 <uzmir6358@gmail.com>
2025-09-25 21:04:54 +0500

    migrated

---
+++ README.md
@@ -0,0 +1,7 @@
+## Django Command Aliases
+
+```bash
+Manage         = python3 manage.py
+Migrate        = python3 manage.py migrate
+Runserver      = python3 manage.py runserver
+Makemigrations = python3 manage.py makemigrations
--- store/models.py
+++ store/models.py
@@ -16,6 +16,7 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
@@ -66,6 +67,7 @@ class Order(models.Model):
 class Adress(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
+    zip = models.CharField(max_length=255)
     customer = models.ForeignKey(Customer, 
                                  on_delete=models.CASCADE)
 
